296f541f6044ac98a3f02dcb6040ce5a749c2cdf
Ashraf <ashrafhussain2265@gmail.com>
2025-06-13 15:57:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import {React,useState,useEffect, use} from 'react';
 import './App.css';
+import CountryCard from './components/CountryCard';
 
 function App() {
+  const[countries , setCountries]=useState([]);
+  useEffect(()=>{
+    const fetchCountries = async ()=>{
+      try{
+           const response = await fetch("https://xcountries-backend.azurewebsites.net/all");
+           const data = await response.json();
+           setCountries(data);
+      }
+      catch(error){
+           console.error("Error fetching data:" , error);
+      }
+      
+    };fetchCountries();
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+       <h1>XCountries</h1>
+    <div className="grid-container">
+      {countries.map((country)=>(
+        <CountryCard key={country.name} name={country.name} flag={country.flag} />
+      ))}
     </div>
+     </div>
   );
 }
 
---
+++ src/components/CountryCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CountryCard({name , flag}){
+    return(
+        <div className="card">
+            <img src={flag} alt={`flag of${name}`} className="flag-image"/>
+            <p className="country-name">{name}</p>
+        </div>
+    );
+};
+export default CountryCard;
