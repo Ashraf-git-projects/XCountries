14f1749ddd558782eb05c78694b86c65a5506573
Ashraf <ashrafhussain2265@gmail.com>
2025-06-13 16:07:20 +0530

    change alt

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -3,7 +3,7 @@ import React from "react";
 function CountryCard({name , flag}){
     return(
         <div className="card">
-            <img src={flag} alt={`flag of${name}`} className="flag-image"/>
+            <img src={flag} alt={`flag of ${name}`} className="flag-image"/>
             <p className="country-name">{name}</p>
         </div>
     );
